0ee80a0a17724e02b5bc148ddd391a1866244407
abuyuk10 <a.buyuk@knomary.com>
2026-01-16 13:38:36 +0100

    removed training

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router';
 import HomeView from './views/HomeView.vue';
-import FlashCardsView from './views/FlashCardsView.vue';
-import PracticeModeView from './views/PracticeModeView.vue';
 import VocabularyView from './views/VocabularyView.vue';
 
 const routes: RouteRecordRaw[] = [
@@ -9,16 +7,6 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'home',
     component: HomeView,
-  },
-  {
-    path: '/training/flash-cards',
-    name: 'flash-cards',
-    component: FlashCardsView,
-  },
-  {
-    path: '/training/practice',
-    name: 'practice',
-    component: PracticeModeView,
   },
   {
     path: '/vocabulary',
